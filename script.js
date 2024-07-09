fdfb116669af5de93f1103cadd042541408e7168
achoo-o <amyj.etana@gmail.com>
2024-07-09 21:42:00 +1200

    Collapsible extra content. bless up

---
+++ script.js
@@ -0,0 +1,13 @@
+
+  const buttons = document.getElementsByClassName("js-collapsible")
+  
+  for(let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      let content = this.nextElementSibling.style
+      if (content.display === "none") {
+        content.display = "flex"
+      } else {
+        content.display = "none"
+      }
+    })
+  }
